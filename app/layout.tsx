0ca82499ee7cd568fac6d17be51bb498e8a3b5a5
jsw <csw9201@naver.com>
2026-06-12 14:17:46 +0900

    feat: app layout metadata 타입 선언 및 한글 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,16 @@
-import { Providers, StyledComponentsRegistry } from '@/app/providers'
-import { Header } from '@/shared/ui/layout'
+import type { Metadata } from 'next'
 
-export const metadata = {
+import { Providers, StyledComponentsRegistry } from '@/app/providers'
+import { Header } from '@/shared'
+
+export const metadata: Metadata = {
   title: {
     default: 'Find Football Player',
     template: '%s | Find Football Player',
   },
-  description: 'Find Football Player',
+  description: '리그를 선택하고 선수 퀴즈를 즐겨보세요.',
+  keywords: ['Find Football Player', '축구', '선수 퀴즈'],
+  authors: [{ name: 'up1' }],
 }
 
 export default function RootLayout({
